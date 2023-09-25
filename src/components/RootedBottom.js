ea0785d3e229a462a3654115e0b4b5be01093a72
chukmas20 <74096185+chukmas20@users.noreply.github.com>
2023-09-25 19:00:52 +0100

    Rootedbottom

---
+++ src/components/RootedBottom.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaRegCopyright  } from "react-icons/fa6";
+
+
+const RootedBottom = () => {
+  return (
+    <div style={{}}>
+    <h3 className='text-white font-semibold flex items-center justify-center '>
+         All rights reserved  <FaRegCopyright className='ml-2' /> <span className='ml-2'>getLinked</span>
+        </h3>
+    </div>
+  )
+}
+
+export default RootedBottom
